0a35c3b03deab1d30cb9c046c6dad434dddccc6e
KercyDing <dkx215417@gmail.com>
2026-08-22 00:44:10 +0800

    chore: remove unused workspace build script

--- build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    #[cfg(target_os = "windows")]
-    {
-        let mut res = winresource::WindowsResource::new();
-        res.set_icon("assets/icon.ico");
-        res.compile().expect("failed to embed Windows icon");
-    }
-}
